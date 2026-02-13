b6a78d1b5fee1ae5757bc593fbe99931641f1ffa
marslan-786 <marslansalfias@gmail.com>
2026-02-13 14:52:52 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "StoreX - VIP Earning",
-  description: "Refer and Earn securely",
+  description: "Secure & Fast Earning Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+      </body>
     </html>
   );
 }
