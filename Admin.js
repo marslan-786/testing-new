6cc1060d9db8e4c904b71463d5a14e4cbc38fedb
marslan-786 <marslansalfias@gmail.com>
2026-02-13 18:06:12 +0500

    Create Admin.js

---
+++ Admin.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const AdminSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, // Plain text for now as requested (admin/admin)
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Admin || mongoose.model("Admin", AdminSchema);
